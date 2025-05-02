466fda19c9fc0da7919f98abf5ee614d888cfe5d
Emmanuel Ajala <ajalae2@gmail.com>
2025-05-02 07:51:30 +0100

    ETL pipeline completed

---
+++ networksecurity/components/data_ingestion.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from networksecurity.exception import NetworkSecurityException
+from networksecurity.logging import logger
+
+from dataclasses import dataclass
+
+
