4199cfd97b913de9bb14e34c150c4595bb43315f
Emmanuel Ajala <ajalae2@gmail.com>
2025-05-09 13:00:29 +0100

    model evaluation and mlflow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -21,6 +21,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier
 )
+import mlflow
 
 
 class ModelTrainer:
@@ -32,6 +33,17 @@ class ModelTrainer:
 
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    def track_mlflow(self, best_model, classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision", precision_score)
+            mlflow.log_metric("recall_score", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
         
     def train_model(self, x_train, y_train, x_test, y_test):
         try:
@@ -92,13 +104,15 @@ class ModelTrainer:
             best_model= models[best_model_name]
 
             y_train_pred= best_model.predict(x_train)
-
             classification_train_metric=get_classification_score(y_true=y_train, y_pred=y_train_pred)
             
-            ## Trach the mlflow
+            ## Track experiment with mlflow
+            self.track_mlflow(best_model, classification_train_metric)
 
             y_test_pred = best_model.predict(x_test)
             classification_test_metric=get_classification_score(y_true=y_test, y_pred=y_test_pred)
+
+            self.track_mlflow(best_model, classification_test_metric)
 
             ## making prediction with your trained model
             preprocessor= load_object(file_path=self.data_transformation_artifact.transformed_obj_file_path)
