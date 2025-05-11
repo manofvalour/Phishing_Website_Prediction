63e0d1994a1db514070303325229cf3a35c1ae6a
Emmanuel Ajala <ajalae2@gmail.com>
2025-05-12 00:25:34 +0100

    s3 bucket

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -8,6 +8,7 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.constants.training_pipeline import TRAINING_BUCKET_NAME
 
 #config_entity
 from networksecurity.entity.config_entity import (
@@ -26,9 +27,11 @@ from networksecurity.entity.arttifact_entity import(
     ModelTrainerArtifact
 )
 
+from networksecurity.cloud.s3_syncer import S3Sync  
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config= TrainingPipelineConfig()
+        self.s3sync= S3Sync()
 
     def start_data_ingestion(self):
         try:
@@ -88,7 +91,24 @@ class TrainingPipeline:
         
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+    
+    ## local artifact going to S3 bucket
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir, aws_bucket_url=aws_bucket_url)
+    
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
         
+
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
 
     def run_pipeline(self):
         try: 
@@ -96,6 +116,11 @@ class TrainingPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
+            
             return model_trainer_artifact
+        
         except Exception as e:
             raise NetworkSecurityException(e,sys)
